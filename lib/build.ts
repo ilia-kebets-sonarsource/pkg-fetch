542ac3c3a4b5bd136d12de606567ab2589b674ef
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-07-25 07:13:57 +0200

    Revert "build: disable NODE_OPTIONS"
    This reverts commit 7f3403bd71d244850ef30a711c52e64f905e9423.

--- lib/build.ts
+++ lib/build.ts
@@ -55,9 +55,6 @@ function getConfigureArgs(major: number, targetPlatform: string): string[] {
     }
   }
 
-  // production binaries do NOT take NODE_OPTIONS from end-users
-  args.push('--without-node-options');
-
   // DTrace
   args.push('--without-dtrace');
 
